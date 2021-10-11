cb7c3a817c3b6c5dd7eecbc3afbb6ae4270af797
aditya77kushwaha <adityakushwaha2912@gmail.com>
2021-10-11 20:51:26 +0530

    Sets up basic express server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
-console.log("Server");
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const cors = require("cors");
+
+app.use(
+    cors({
+        origin: "*",
+        credentials: true,
+    })
+);
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json());
+
+const port = 30001
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.json({got:"true"})
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
